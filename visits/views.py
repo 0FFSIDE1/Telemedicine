79dbe1cb15c85023ea3920a0c093835da7c53c3b
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-09-05 18:38:10 +0100

    Feat: all visits and detail visits

--- visits/views.py
+++ visits/views.py
@@ -1,3 +1,31 @@
+from django.http import Http404
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from services.visits_services import *
+from visits.models import Visit
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
+class AllVisitsView(APIView):
+    def get(self, request):
+        visit = Visit.objects.all()
+        serializer = VisitSerializer(visit, many=True)
+        data = {
+            'status': 'success',
+            'message': 'All Visitation data retrieved successfully',
+            'patients': serializer.data,  
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
+    
+class VisitView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Visit.objects.all()
+    serializer_class = VisitSerializer
+    def get_object(self):
+       try:
+           return super().get_object()
+       except Http404:
+           raise NotFound(detail="Visit record not found with the provided ID.", code=404)
+       
