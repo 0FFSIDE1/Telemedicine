58ec4066012dd4438f7b632c7df7b1024733e69c
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-09-05 18:38:28 +0100

    fix: url config for visits

--- visits/urls.py
+++ visits/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from .views import *
 urlpatterns = [
-    
+    path('api/v1/visitation', AllVisitsView.as_view(), name='all-visits'),
+    path('api/v1/<int:pk>/visitation', VisitView.as_view(), name='visit-detail')
 ]
+    
