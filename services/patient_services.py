22a993b066d9afdc7b524135eb31fca47c1ab1e1
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-09-05 18:37:37 +0100

    fix: updated

--- services/patient_services.py
+++ services/patient_services.py
@@ -1,15 +1,18 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework import serializers
 from patients.models import Patient
 from visits.models import Visit
 from django.contrib import messages
 from .visits_services import VisitSerializer
+from rest_framework import status
+
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = '__all__'
-
+    
 class PatientVisitSerializer(serializers.ModelSerializer):
     visits = serializers.SerializerMethodField()
     class Meta:
@@ -19,32 +22,12 @@ class PatientVisitSerializer(serializers.ModelSerializer):
         visits = Visit.objects.filter(patient=obj)
         return VisitSerializer(visits)
 
-
-
-
-
 def create_patient_record(request, data):
     try:
         patient = Patient.objects.create(**data)
     except Exception as e:
         messages.error(request, f'{e}')
 
-def delete_patient_record(request, id):
-    try:
-        patient = Patient.objects.get(patient_id=id)
-        patient.delete()
-    except Exception as e:
-        messages.error(request, f'Error deleting record {e}')
-
-def get_patient_record(id):
-    try:
-        patient = Patient.objects.get(patient_id=id)
-        data = {
-            'patient': PatientSerializer(patient)
-        }
-        return JsonResponse()
-    except Exception as e:
-        pass
 
 
     
