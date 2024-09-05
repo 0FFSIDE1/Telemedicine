c5473454adfc824b7ee64d21006a25faf743632b
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-09-05 18:37:15 +0100

    fix: url config for patient view

--- patients/urls.py
+++ patients/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/v1/patients', PatientView.as_view(), name='all-patient')
+    path('api/v1/patients', AllPatientView.as_view(), name='all-patient'),
+    path('api/v1/<int:pk>/patient', PatientView.as_view(), name='patient')
 ]
