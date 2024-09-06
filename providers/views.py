3da3c9e7b8827017640f607f0e78f966ace136d8
0FFSIDE1 <somtoemmanuel6@gmail.com>
2024-09-06 10:30:25 +0100

    feat: Get, Put, Patch, Delete

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,39 @@
+from django.http import Http404
 from django.shortcuts import render
-
+# from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from services.provider_services import *
+from rest_framework import generics
+from rest_framework.exceptions import NotFound
 # Create your views here.
+
+class AllProviderView(generics.ListCreateAPIView):
+        """
+        GET: Retrieves all provider record
+        POST: Add new provider record
+        queryset: All column in Provider Entity
+        """
+        queryset = Provider.objects.all()
+        serializer_class = ProviderSerializer
+       
+class ProviderView(generics.RetrieveUpdateDestroyAPIView):
+    """
+       GET: Retrieves specific provider record
+       PUT and PATCH: update provider record
+       DELETE: delete specific provider record
+       queryset: all column in Provider Entity
+    """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound(detail="Provider not found with the provided ID.", code=404)
+       
+    # def check_permissions(self, request):
+    #     if not request.user.has_perm('patients.change_Patient'):
+    #         raise PermissionDenied({"error": "You do not have permission to update this object."})
+    #     return super().check_permissions(request)
+
--- visits/views.py
+++ visits/views.py
@@ -1,31 +1,37 @@
 from django.http import Http404
 from django.shortcuts import render
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from services.visits_services import *
+from services.patient_services import PatientVisitSerializer
 from visits.models import Visit
 from rest_framework import generics
 from rest_framework.exceptions import NotFound
 
 # Create your views here.
-class AllVisitsView(APIView):
-    def get(self, request):
-        visit = Visit.objects.all()
-        serializer = VisitSerializer(visit, many=True)
-        data = {
-            'status': 'success',
-            'message': 'All Visitation data retrieved successfully',
-            'patients': serializer.data,  
-        }
-        return Response(data=data, status=status.HTTP_200_OK)
-    
-class VisitView(generics.RetrieveUpdateDestroyAPIView):
+class AllVisitsView(generics.ListCreateAPIView):
+    """
+       GET: Retrieves all visits record
+       POST: Add new visit record
+       queryset: All column in visit Entity
+    """
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
+    
+    
+class VisitView(generics.RetrieveUpdateDestroyAPIView):
+    """
+       GET: Retrieves specific visits record
+       PUT and PATCH: update visit record
+       DELETE: delete specific visit record
+       queryset: all column in visit Entity
+    """
+    queryset = Visit.objects.all()
+    serializer_class = PatientVisitSerializer
     def get_object(self):
-       try:
-           return super().get_object()
-       except Http404:
-           raise NotFound(detail="Visit record not found with the provided ID.", code=404)
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound(detail="Visit record not found with the provided ID.", code=404)
        
